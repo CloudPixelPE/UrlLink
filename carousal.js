8160040d3597fa04de908ee040764320da6890b6
AnasMunir <anas.munir.92@gmail.com>
2016-07-07 23:54:40 +0500

    commit for Maan

---
+++ carousal.js
@@ -0,0 +1,10 @@
+// When the DOM is ready, run this function
+$(document).ready(function() {
+    //Set the carousel options
+    $('#quote-carousel').carousel({
+        pause: true,
+        interval: 4000,
+    });
+});/**
+ * Created by Anas on 7/7/2016.
+ */
